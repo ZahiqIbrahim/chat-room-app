662fb796641b2488ddc2b43da2f5a562d52b3d1b
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-22 13:10:14 +0530

    message method

---
+++ src/main/java/com/example/chat_room_app/MessageController.java
@@ -0,0 +1,24 @@
+package com.example.chat_room_app;
+
+import com.example.chat_room_app.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message){
+
+        try{
+            //processing
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return message;
+    }
+}
---
+++ src/main/java/com/example/chat_room_app/model/Message.java
@@ -0,0 +1,29 @@
+package com.example.chat_room_app.model;
+
+public class Message {
+
+    private String name;
+
+    public Message(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+}
