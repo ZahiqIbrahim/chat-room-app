78b21f95498cdf0e03a0eded184df6ae1cd97571
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-22 12:49:57 +0530

    Initial commit

---
+++ src/test/java/com/example/chat_room_app/ChatRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_room_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
